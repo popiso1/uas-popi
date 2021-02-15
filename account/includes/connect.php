57fe6ec2842970666e7449b56105d7b3bf3fb7ff
popiso1 <oktavianipopi1@gmail.com>
2021-02-15 00:00:05 -0800

    commit tugas uas

---
+++ account/includes/connect.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+$servername = "localhost";
+$server_user = "root";
+$server_pass = "";
+$dbname = "food";
+$name = $_SESSION['name'];
+$role = $_SESSION['role'];
+$con = new mysqli($servername, $server_user, $server_pass, $dbname);
+if(!$con){
+	echo "Please Check connect File..";
+}
+?>
